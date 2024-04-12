85cfbbf4beac187af129ec0f0355ad150a67e934
JAIRO <Jairo04molina75@gmail>
2024-04-12 17:03:15 -0600

    Agregar archivos de proyecto.

---
+++ POO/CLASES/Carro.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO.CLASES
+{
+    internal class Carro
+    {
+        public string Marca { get; }
+        public string Modelo { get; }
+        public int Año { get; }
+        public string Transmision { get; }
+        public string Color { get; set; }
+        public string Owner { get; set; }
+
+        public bool Encendido = false;
+        public bool Seguro = false;
+        public bool Alarma = false;
+
+        public bool Parking = true;
+        public bool Reverse = false;
+        public bool Neutro = false;
+        public bool Drive = false;
+
+
+        public int Velocidad_Actual { get; set; }
+        public int Velocidad_Max { get; set; }
+        public int Velocidad_Max_Reversa { get; set; }
+
+
+        string Cambios_Car = "";
+        string Cambio_Actual = "P";
+
+        public Carro (string marc, string mode, int año, string tran)
+        {
+            Marca = marc;
+            Modelo = mode;
+            Año = año;
+            Transmision = tran;
+            Velocidad_Max = 120;
+            Velocidad_Max_Reversa = Velocidad_Max / 2;
+        }
+
+        public void Encender()
+        {
+            Encendido = true;
+
+        }
+
+        public void Apagar()
+        {
+            if (Encendido && Velocidad_Actual == 0 && Cambio_Actual == "P")
+            {
+                Encendido = false;
+            }
+        }
+
+        public string Caja_Cambios()
+        {
+            Cambios_Car = Console.ReadLine().ToUpper();
+
+            switch (Cambios_Car)
+            {
+                case "P":
+                    return Cambio_Actual = "P";
+                    break;
+
+                case "R":
+                    return Cambio_Actual = "R";
+                    break;
+
+                case "N":
+                    return Cambio_Actual = "N";
+                    break;
+
+                case "D":
+                    return Cambio_Actual = "D";
+                    break;
+
+                default:
+                    return "Error";
+            }
+        }
+
+        public int Acelerar()
+        {
+            if (!Encendido)
+            {
+                return Velocidad_Actual;
+            }
+
+            if (Encendido && Cambio_Actual == "D")
+            {
+                Velocidad_Actual += 15;
+
+                if (Velocidad_Actual > Velocidad_Max)
+                {
+                    Velocidad_Actual = Velocidad_Max;
+                }
+                return Velocidad_Actual;
+            }
+
+            if (Encendido && Cambio_Actual == "R")
+            {
+                Velocidad_Actual += 10;
+
+                if (Velocidad_Actual > Velocidad_Max_Reversa)
+                {
+                    Velocidad_Actual = Velocidad_Max_Reversa;
+                }
+                return Velocidad_Actual;
+            }
+            return Velocidad_Actual;
+        }
+
+        public int Frenar()
+        {
+            Velocidad_Actual -= Velocidad_Max;
+
+            if (Velocidad_Actual <= 0)
+            {
+                return Velocidad_Actual = 0;
+            }
+            return Velocidad_Actual;
+        }
+
+        public int Desacelerar()
+        {
+            Velocidad_Actual -= 10;
+
+            if (Velocidad_Actual <= 0)
+            {
+                return Velocidad_Actual = 0;
+            }
+            return Velocidad_Actual;
+        }
+    }
+}
---
+++ POO/PROGRAM.cs
@@ -0,0 +1,82 @@
+﻿using POO.CLASES;
+
+Carro Car1 = new Carro ("TOYOTA", "TACOMA", 2002, "Automatico");
+
+Car1.Color = "ROJO";
+Car1.Owner = "JUAN";
+
+//Agrgar case o una forma mas ordenada de ejecutar el programa
+
+Console.WriteLine ("¿Desea ver la información del carro? \n(Si = S | No = N): ");
+string Info_Car = Console.ReadLine().ToUpper();
+
+if (Info_Car == "S")
+{
+    Console.WriteLine("Marca: " + Car1.Marca);
+    Console.WriteLine("Marca: " + Car1.Modelo);
+    Console.WriteLine("Año: " + Car1.Año);
+    Console.WriteLine("Transmisión: " + Car1.Transmision);
+    Console.WriteLine("Velocidad Máxima: " + Car1.Velocidad_Max + "/KPH");
+    Console.WriteLine("Velocidad Máxima (Reversa): " + Car1.Velocidad_Max_Reversa + "/KPH");
+    Console.WriteLine("Transmisión: " + Car1.Transmision);
+
+
+    Console.WriteLine("\nPresiona cualquier tecla para continuar...");
+    Console.ReadKey();
+}
+else if (Info_Car == "N")
+{
+
+}
+else
+{
+    Console.WriteLine("\nOpción Invalida");
+}
+
+Console.WriteLine("¿Desea encender el carro? \n(Si = S | No = N): ");
+string Estado_Car = Console.ReadLine().ToUpper();
+
+if (Estado_Car == "S")
+{
+    Car1.Encender();
+    Console.WriteLine("\nCarro Encendido");
+
+}
+else if (Estado_Car == "N")
+{
+    Car1.Apagar();
+    Console.WriteLine("\nCarro Apagado");
+
+}
+else
+{
+    Console.WriteLine("\nOpción Invalida");
+}
+
+Console.WriteLine("¿Desea Hacer Un Cambio? \n(Si = S | No = N): ");
+string Cambio_Car = Console.ReadLine().ToUpper();
+if (Cambio_Car == "S")
+{
+    Console.WriteLine("(Parking = P | Reverse = R | Neutro = N | Drive = D): "); //Arreglar la presentación
+    Car1.Caja_Cambios();
+
+}
+else if (Cambio_Car == "N")
+{
+
+}
+else
+{
+    Console.WriteLine("\nOpción Invalida");
+}
+
+Console.WriteLine(Car1.Acelerar() + "/KPH");
+//Console.WriteLine(Car1.Frenar() + "/KPH");
+Console.WriteLine(Car1.Acelerar() + "/KPH");
+Console.WriteLine(Car1.Acelerar() + "/KPH");
+Console.WriteLine(Car1.Acelerar() + "/KPH");
+//Console.WriteLine(Car1.Frenar() + "/KPH");
+Console.WriteLine(Car1.Acelerar() + "/KPH");
+Console.WriteLine(Car1.Acelerar() + "/KPH");
+Console.WriteLine(Car1.Acelerar() + "/KPH");
+Console.WriteLine(Car1.Acelerar() + "/KPH");
